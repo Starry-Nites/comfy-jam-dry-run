28c5cbf0e67c74172d6ad4e05a0c12f614e93a06
starry-nites <jacjack09@outlook.com>
2026-05-30 15:01:52 -0500

    Animations and movement are now working. Use arrow keys to move character.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 30.0
 const JUMP_VELOCITY = -400.0
 
 @onready var _animated_sprite = $AnimatedSprite2D
@@ -13,18 +13,19 @@ func _physics_process(delta: float) -> void:
 	var y_direction : int 
 	var animation = ""
 	if Input.is_action_pressed("ui_right"):
+		print("Put a dope ass wizard tower on it")
 		x_direction = 1
 		animation = "walk_right"
-	elif Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
 		x_direction = -1
 		animation = "walk_left"
 	
 	if Input.is_action_pressed("ui_down"):
-		y_direction = -1
+		y_direction = 1
 		animation = "walk_front"
 	
-	elif Input.is_action_pressed("ui_up"):
-		y_direction = 1
+	if Input.is_action_pressed("ui_up"):
+		y_direction = -1
 		animation = "walk_back"
 		
 	var buttons_pressed: bool = (
@@ -35,13 +36,16 @@ func _physics_process(delta: float) -> void:
 	)
 	
 	if ! buttons_pressed:
+		print("THIS SHIT SUCKS.")
 		x_direction = 0
 		y_direction = 0
+		animation = "idle_front"
 	
-	velocity.x = x_direction * SPEED
-	velocity.y = y_direction * SPEED
+	_animated_sprite.play(animation)
+	position.x += x_direction * SPEED * delta
+	position.y += y_direction * SPEED * delta
 	
-	velocity.x = move_toward(velocity.x, 0, SPEED)
-	velocity.y = move_toward(velocity.y, 0, SPEED)
+	#velocity.x = move_toward(velocity.x, 0, SPEED)
+	#velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
