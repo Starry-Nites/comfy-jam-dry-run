cc50a795e722c4172b1e96a4205f67d45cb87d47
starry-nites <jacjack09@outlook.com>
2026-05-31 08:31:19 -0500

    made player movement better and added background on main_scene.

---
+++ scenes/inventory/inventory.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var visible = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func setVisible(v : bool) -> void:
+	visible = v
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+		
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 30.0
+const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
 
 var last_dir = ""
@@ -16,32 +16,49 @@ func _physics_process(delta: float) -> void:
 	var y_direction : int 
 	var animation = ""
 	
-	if Input.is_action_pressed("ui_right"):
-		#print("Put a dope ass wizard tower on it")
-		x_direction = 1
+	#region Movement Controls
+	var direction := Vector2.ZERO
+
+	if Input.is_action_pressed("walk_right"):
 		animation = "walk_right"
 		last_dir = "right"
-		
-	if Input.is_action_pressed("ui_left"):
-		x_direction = -1
+		direction.x += 1
+	if Input.is_action_pressed("walk_left"):
 		animation = "walk_left"
 		last_dir = "left"
-		
-	if Input.is_action_pressed("ui_down"):
-		y_direction = 1
+		direction.x -= 1
+	if Input.is_action_pressed("walk_forward"):
 		animation = "walk_front"
 		last_dir = "front"
-		
-	if Input.is_action_pressed("ui_up"):
-		y_direction = -1
+		direction.y += 1
+	if Input.is_action_pressed("walk_back"):
 		animation = "walk_back"
 		last_dir = "back"
+		direction.y -= 1
+		
+	velocity = direction.normalized() * SPEED
+	#if Input.is_action_pressed("ui_right"):
+		##print("Put a dope ass wizard tower on it")
+		#x_direction = 1
+		#
+		#
+	#if Input.is_action_pressed("ui_left"):
+		#x_direction = -1
+		#
+		#
+	#if Input.is_action_pressed("ui_down"):
+		#y_direction = 1
+		#
+		#
+	#if Input.is_action_pressed("ui_up"):
+		#y_direction = -1
+		
 		
 	var buttons_pressed: bool = (
-	Input.is_action_pressed("ui_right") || 
-	Input.is_action_pressed("ui_left") || 
-	Input.is_action_pressed("ui_down") || 
-	Input.is_action_pressed("ui_up")
+	Input.is_action_pressed("walk_right") || 
+	Input.is_action_pressed("walk_left") || 
+	Input.is_action_pressed("walk_forward") || 
+	Input.is_action_pressed("walk_back")
 	)
 	
 	if ! buttons_pressed:
@@ -49,9 +66,14 @@ func _physics_process(delta: float) -> void:
 		x_direction = 0
 		y_direction = 0
 		animation = "idle_" + last_dir
+	#endregion
 	
 	_animated_sprite.play(animation)
 	position.x += x_direction * SPEED * delta
 	position.y += y_direction * SPEED * delta
+	
+	# Inventory Controls
+	#if Input.is_action_pressed("inventory"):
+		
 
 	move_and_slide()
