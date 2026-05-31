7997b997e5ae943c4c21731db635e09140fba934
starry-nites <jacjack09@outlook.com>
2026-05-30 21:56:09 -0500

    Fixed idle animations

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,7 +4,10 @@ extends CharacterBody2D
 const SPEED = 30.0
 const JUMP_VELOCITY = -400.0
 
+var last_dir = ""
+
 @onready var _animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
@@ -12,21 +15,27 @@ func _physics_process(delta: float) -> void:
 	var x_direction : int
 	var y_direction : int 
 	var animation = ""
+	
 	if Input.is_action_pressed("ui_right"):
-		print("Put a dope ass wizard tower on it")
+		#print("Put a dope ass wizard tower on it")
 		x_direction = 1
 		animation = "walk_right"
+		last_dir = "right"
+		
 	if Input.is_action_pressed("ui_left"):
 		x_direction = -1
 		animation = "walk_left"
-	
+		last_dir = "left"
+		
 	if Input.is_action_pressed("ui_down"):
 		y_direction = 1
 		animation = "walk_front"
-	
+		last_dir = "front"
+		
 	if Input.is_action_pressed("ui_up"):
 		y_direction = -1
 		animation = "walk_back"
+		last_dir = "back"
 		
 	var buttons_pressed: bool = (
 	Input.is_action_pressed("ui_right") || 
@@ -36,16 +45,13 @@ func _physics_process(delta: float) -> void:
 	)
 	
 	if ! buttons_pressed:
-		print("THIS SHIT SUCKS.")
+		#print("THIS SHIT SUCKS.")
 		x_direction = 0
 		y_direction = 0
-		animation = "idle_front"
+		animation = "idle_" + last_dir
 	
 	_animated_sprite.play(animation)
 	position.x += x_direction * SPEED * delta
 	position.y += y_direction * SPEED * delta
-	
-	#velocity.x = move_toward(velocity.x, 0, SPEED)
-	#velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
